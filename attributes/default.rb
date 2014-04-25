d4b91bea47293739bd16bc0a3bdd8db29d02c7dd
alisdair sullivan <alisdairsullivan@yahoo.ca>
2014-04-24 18:47:17 -0700

    update default elixir release to 0.13

--- attributes/default.rb
+++ attributes/default.rb
@@ -21,4 +21,4 @@
 # THE SOFTWARE.
 
 default['elixir']['elixir_git_url'] = "https://github.com/elixir-lang/elixir"
-default['elixir']['elixir_git_ref'] = "v0.12.0"
+default['elixir']['elixir_git_ref'] = "v0.13.0"
